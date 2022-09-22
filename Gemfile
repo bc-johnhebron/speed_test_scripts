acd941445a523dadc20911d106ad5a60b5ce697f
John Hebron <johnhebron@gmail.com>
2022-09-22 01:00:38 -0500

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,2 @@
+source 'https://rubygems.org'
+gem 'speedtest_net', git: "https://github.com/bc-johnhebron/speedtest_net.git", branch: "add-hash-and-array-methods"
---
+++ speedtest.rb
@@ -0,0 +1,28 @@
+require 'irb'
+require 'speedtest_net'
+
+def run_test
+  time = Time.now
+  result = SpeedtestNet.run
+
+  open('files/network_speeds.csv', 'a') do |f|
+    f.puts "'#{time}',#{result.pretty_download},#{result.pretty_upload},#{result.pretty_latency}"
+  end
+
+  # open('network_speeds_full.csv', 'a') do |f|
+  #   f.puts "Time," + result.array_keys.join(',')
+  # end
+
+  open('files/network_speeds_full.csv', 'a') do |f|
+    f.puts "#{time}," + result.to_array.join(',')
+  end
+
+  puts "[#{Time.now}] Download: #{result.pretty_download}. Upload: #{result.pretty_upload}. Latency: #{result.pretty_latency}"
+  result
+end
+
+run_test
+
+
+# Start IRB
+# IRB.start(__FILE__)
